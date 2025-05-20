9c505f97492779c243319908f9426b9388d1dadd
Tom Bar <og.tombar@gmail.com>
2025-05-20 14:50:08 +0300

    changed from Flask to FastAPI for easier concurrency

---
+++ backend/app.py
@@ -0,0 +1,43 @@
+"""Backend for the application."""
+
+import socketio
+import uvicorn
+from app_manager import AppManager
+from data_models import JoinGameData
+from events import ClientEvent
+from fastapi import FastAPI
+
+sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
+app = FastAPI()
+manager = AppManager()
+
+
+@sio.on(ClientEvent.JOIN_LOBBY)
+async def handle_join_lobby(sid: str, _: dict):
+    """Handles a player joining the lobby."""
+    print(f"[backend] {sid} joined lobby")
+    # manager.add_player(sid)
+
+
+@sio.on(ClientEvent.JOIN_GAME)
+async def handle_join_game(sid: str, data: dict):
+    """Handles a player joining a game.
+
+    Args:
+        data (dict): The data from the client.
+    """
+    event_data = JoinGameData.from_dict(data)
+    print(f"[backend] {sid} joined game {event_data.game_id}")
+    # manager.join_game(sid, event_data)
+
+
+@sio.on(ClientEvent.DISCONNECT)
+async def handle_disconnect(sid: str):
+    """Handles a player disconnecting from the server."""
+    print(f"[backend] {sid} disconnected")
+    # manager.remove_player(sid)
+
+
+if __name__ == "__main__":
+    sio_app = socketio.ASGIApp(sio, other_asgi_app=app)
+    uvicorn.run(sio_app, host="0.0.0.0", port=8000)
--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("http://localhost:5000", {
+const socket: Socket = io("http://localhost:8000", {
   transports: ["websocket"],
 });
 
